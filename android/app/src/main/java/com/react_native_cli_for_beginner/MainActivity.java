0a94e1739ef495adf888ed8ee5c911a2790a7403
dk-cell <singhdpk009@gmail.com>
2022-01-14 20:45:46 +0530

    initial commit

---
+++ android/app/src/main/java/com/react_native_cli_for_beginner/MainActivity.java
@@ -0,0 +1,15 @@
+package com.react_native_cli_for_beginner;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "React_Native_CLI_For_Beginner";
+  }
+}
